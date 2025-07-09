8aeadef001cfb66e4371f9ac613484eb1b15c21d
camiloarevalog <cristianarevalogutierrez@gmail.com>
2025-07-09 14:37:44 -0500

    Update

---
+++ src/main/java/com/supermarket/clients/Entity/ClientEntity.java
@@ -0,0 +1,4 @@
+package com.supermarket.clients.Entity;
+
+public class ClientEntity {
+}
--- src/main/java/com/supermarket/clients/repository/ClientRepository.java
+++ src/main/java/com/supermarket/clients/repository/ClientRepository.java
@@ -1,5 +1,6 @@
 package com.supermarket.clients.repository;
 
+import com.supermarket.clients.Entity.ClientEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClientRepository extends JpaRepository<ClientEntity, Long> {
